6d333b3ef3c939962fee1c5c9ac2c7207c017069
Yifan Zhu <zhuyi@tcd.ie>
2022-12-29 22:09:52 +0000

    Update 0043. Multiply Strings.java

--- 0043. Multiply Strings.java
+++ 0043. Multiply Strings.java
@@ -1,23 +1,30 @@
 class Solution {
     public String multiply(String num1, String num2) {
-        int n1 = num1.length(), n2 = num2.length();
-        int[] products = new int[n1 + n2];
-        for (int i = n1 - 1; i >= 0; i--) {
-            for (int j = n2 - 1; j >= 0; j--) {
-                int d1 = num1.charAt(i) - '0';
-                int d2 = num2.charAt(j) - '0';
-                products[i + j + 1] += d1 * d2;
+        int[] product = new int[num1.length() + num2.length()];
+
+        for (int i = num1.length() - 1; i >= 0; i--) {
+            for (int j = num2.length() - 1; j >= 0; j--) {
+                int digitProduct = (num1.charAt(i) - '0') * (num2.charAt(j) - '0');
+                product[i + j + 1] += digitProduct % 10;
+                if (product[i + j + 1] >= 10) {
+                    product[i + j + 1] -= 10;
+                    product[i + j]++;
+                }
+                product[i + j] += digitProduct / 10;
+                if (product[i + j] >= 10) {
+                    product[i + j] -= 10;
+                    product[i + j - 1]++;
+                }
             }
         }
-        int carry = 0;
-        for (int i = products.length - 1; i >= 0; i--) {
-            int tmp = (products[i] + carry) % 10;
-            carry = (products[i] + carry) / 10;
-            products[i] = tmp;
-        }
+
         StringBuilder sb = new StringBuilder();
-        for (int num : products) sb.append(num);
-        while (sb.length() != 0 && sb.charAt(0) == '0') sb.deleteCharAt(0);
-        return sb.length() == 0 ? "0" : sb.toString();
+        for (int digit : product) {
+            sb.append(digit);
+        }
+        while (!sb.isEmpty() && sb.charAt(0) == '0') {
+            sb.deleteCharAt(0);
+        }
+        return sb.isEmpty() ? "0" : sb.toString();
     }
 }
