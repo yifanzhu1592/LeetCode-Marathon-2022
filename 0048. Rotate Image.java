502452eb11c458119d53dd1365b45f6dfd68e6be
Yifan Zhu <zhuyi@tcd.ie>
2022-12-29 22:35:34 +0000

    Update 0048. Rotate Image.java

--- 0048. Rotate Image.java
+++ 0048. Rotate Image.java
@@ -1,15 +1,13 @@
 class Solution {
     public void rotate(int[][] matrix) {
-        int firstRow = 0, lastRow = matrix.length - 1;
-        while(firstRow < lastRow){
-            int[] temp = matrix[firstRow];
-            matrix[firstRow] = matrix[lastRow];
-            matrix[lastRow] = temp;
-            firstRow++; lastRow--;
+        for (int i = 0, j = matrix.length - 1; i < j; i++, j--) {
+            int[] temp = matrix[i];
+            matrix[i] = matrix[j];
+            matrix[j] = temp;
         }
 
-        for(int i = 0; i < matrix.length; i++){
-            for(int j = i+1; j < matrix[i].length; j++){
+        for (int i = 0; i < matrix.length; i++) {
+            for (int j = i + 1; j < matrix[0].length; j++) {
                 int temp = matrix[i][j];
                 matrix[i][j] = matrix[j][i];
                 matrix[j][i] = temp;
