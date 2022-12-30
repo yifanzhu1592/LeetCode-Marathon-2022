dc0330f8218f177e95484a3a6dc6dce3024e8144
Yifan Zhu <zhuyi@tcd.ie>
2022-12-30 21:02:03 +0000

    Update 0200. Number of Islands.java

--- 0200. Number of Islands.java
+++ 0200. Number of Islands.java
@@ -1,30 +1,33 @@
 class Solution {
-
     public int numIslands(char[][] grid) {
-        
         if (grid.length == 0) {
             return 0;
         }
+
         int count = 0;
-        
-        for (int i = 0; i < grid.length; i++){
-            for (int j = 0; j < grid[0].length; j++)
+
+        for (int i = 0; i < grid.length; i++) {
+            for (int j = 0; j < grid[0].length; j++) {
                 if (grid[i][j] == '1') {
-                    DFS(grid, i, j);
-                    ++count;
+                    dfs(grid, i, j);
+                    count++;
                 }
-        }    
+            }
+        }
+
         return count;
     }
 
-    private void DFS(char[][] grid, int i, int j) {
-        if (i < 0 || j < 0 || i >= grid.length || j >= grid[0].length || grid[i][j] != '1') {
+    private void dfs(char[][] grid, int i, int j) {
+        if (i < 0 || i >= grid.length || j < 0 || j >= grid[0].length || grid[i][j] == '0') {
             return;
         }
+
         grid[i][j] = '0';
-        DFS(grid, i + 1, j);
-        DFS(grid, i - 1, j);
-        DFS(grid, i, j + 1);
-        DFS(grid, i, j - 1);
+
+        dfs(grid, i, j + 1);
+        dfs(grid, i, j - 1);
+        dfs(grid, i + 1, j);
+        dfs(grid, i - 1, j);
     }
 }
