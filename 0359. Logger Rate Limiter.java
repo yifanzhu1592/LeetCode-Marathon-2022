fc81f46e3e5d5613a9b6aad774fbaeb9b5325c4f
Yifan Zhu <zhuyi@tcd.ie>
2022-12-31 17:32:12 +0000

    Create 0359. Logger Rate Limiter.java

---
+++ 0359. Logger Rate Limiter.java
@@ -0,0 +1,23 @@
+class Logger {
+
+    Map<String, Integer> map;
+
+    public Logger() {
+        map = new HashMap<>();
+    }
+    
+    public boolean shouldPrintMessage(int timestamp, String message) {
+        if (!map.containsKey(message) || (timestamp >= map.get(message))) {
+            map.put(message, timestamp + 10);
+            return true;
+        } else {
+            return false;
+        }
+    }
+}
+
+/**
+ * Your Logger object will be instantiated and called as such:
+ * Logger obj = new Logger();
+ * boolean param_1 = obj.shouldPrintMessage(timestamp,message);
+ */
