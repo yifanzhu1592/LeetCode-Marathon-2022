fc297c64f408cdfea032793bebfa6946875d3b4a
Yifan Zhu <zhuyi@tcd.ie>
2023-01-01 17:55:08 +0000

    Update 0490. The Maze.java

--- 0490. The Maze.java
+++ 0490. The Maze.java
@@ -1,33 +1,35 @@
 class Solution {
-    
     int[][] directions = {{0, 1}, {0, -1}, {1, 0}, {-1, 0}};
-    
-    private boolean dfs(int[][] maze, int[] current, int[] destination, boolean[][] visited) {
-        if (current[0] == destination[0] && current[1] == destination[1]) {
+
+    public boolean hasPath(int[][] maze, int[] start, int[] destination) {
+        return dfs(maze, start, destination);
+    }
+
+    private boolean dfs(int[][] maze, int[] current, int[] destination) {
+        int row = current[0];
+        int col = current[1];
+        if (row == destination[0] && col == destination[1]) {
             return true;
         }
-        int row = current[0], col = current[1];
-        if (row < 0 || row > maze.length || col < 0 || col > maze[0].length || visited[row][col])  {
+        int rows = maze.length;
+        int cols = maze[0].length;
+        if (row < 0 || row >= rows || col < 0 || col >= cols || maze[row][col] == -1) {
             return false;
         }
-        visited[row][col] = true;
+        maze[row][col] = -1;
+
         for (int i = 0; i < directions.length; i++) {
-            int currentRow = row, currentCol = col;
-            while (currentRow + directions[i][0] >= 0 && currentRow + directions[i][0] < maze.length && currentCol + directions[i][1] >= 0 && currentCol + directions[i][1] < maze[0].length && maze[currentRow + directions[i][0]][currentCol + directions[i][1]] == 0) {
+            int currentRow = row;
+            int currentCol = col;
+            while (currentRow + directions[i][0] >= 0 && currentRow + directions[i][0] < rows && currentCol + directions[i][1] >= 0 && currentCol + directions[i][1] < cols && maze[currentRow + directions[i][0]][currentCol + directions[i][1]] != 1) {
                 currentRow += directions[i][0];
                 currentCol += directions[i][1];
             }
-            if (dfs(maze, new int[]{currentRow, currentCol}, destination, visited)) {
+            if (dfs(maze, new int[]{currentRow, currentCol}, destination)) {
                 return true;
             }
         }
+
         return false;
-    }
-    
-    public boolean hasPath(int[][] maze, int[] start, int[] destination) {
-        if (maze.length == 0 && maze[0].length == 0) {
-            return false;
-        }
-        return dfs(maze, start, destination, new boolean[maze.length][maze[0].length]);
     }
 }
